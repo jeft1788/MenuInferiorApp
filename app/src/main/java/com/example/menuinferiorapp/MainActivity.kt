e96c44b8e44b636aa6f56470323355ffd4635ca4
jeft1788 <jeft1788@gmail.com>
2023-10-18 13:15:04 -0500

    notficacion en menu

--- app/src/main/java/com/example/menuinferiorapp/MainActivity.kt
+++ app/src/main/java/com/example/menuinferiorapp/MainActivity.kt
@@ -11,33 +11,52 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
     lateinit var toolbar: ActionBar
      private lateinit var binding : ActivityMainBinding
+     lateinit var botonNav: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+        botonNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        //binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(binding.root)
         val menu: Menu
 
         //val bottomNavigation: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        setupBottomNavigationView()
         replaceFragment(FragmentHome())
-        binding.bottomNavigationView.setOnClickListener {
-            when(it.id){
+        botonNav.setOnItemSelectedListener {
+            when(it.itemId){
 
-                R.id.home -> replaceFragment(FragmentHome())
-                R.id.profile -> replaceFragment(FragmentProfile())
+
+                R.id.profile -> {
+                    removeBadge(it.itemId)
+                    replaceFragment(FragmentProfile())
+                }
                 R.id.settings -> replaceFragment(FragmentSettings())
 
                 else ->{
-
+                    replaceFragment(FragmentHome())
                 }
-
             }
-
             true
 
         }
 
+    }
+
+    private fun setupBottomNavigationView() {
+        botonNav.getOrCreateBadge(R.id.profile)
+        botonNav.getBadge(R.id.profile)?.apply {
+            number = 10
+        }
+    }
+
+    private fun removeBadge(badgeId: Int) {
+        botonNav.getBadge(badgeId)?.let { badgeDrawable ->
+            if (badgeDrawable.isVisible) {
+               botonNav.removeBadge(badgeId)
+            }
+        }
     }
     private fun replaceFragment(fragment : Fragment){
 
